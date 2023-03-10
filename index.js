c6381893a7da096aed958b4524b0ff176f701b5e
Careless-Whisper999 <chdhoucheng@gmail.com>
2023-03-10 15:43:41 -0600

    add icon

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* TODO:
  1. factor out common code in event listeners for better code reuse and maintainability
- 2. move the green button under completed taks to the left to match with the design
+ 2. move the green button under completed tasks to the left to match with the design
 */
 
-// My implementation of myFetch
+// My implementation of fetch
 function myFetch(url, options = {}) {
   return new Promise((res, rej) => {
     let xhr = new XMLHttpRequest();
@@ -102,11 +102,11 @@ const View = (() => {
       return todo.completed;
     });
     todospending.forEach((todo) => {
-      const liTemplate = `<li><span id="edit/${todo.id}">${todo.content}</span><button class="edit-btn" id="edit-btn/${todo.id}">edit</button><button class="delete-btn" id="delete-btn/${todo.id}">delete</button><button class="move-btn" id="move-btn/${todo.id}">move</button></li>`;
+      const liTemplate = `<li><span id="edit/${todo.id}">${todo.content}</span><button class="edit-btn" id="edit-btn/${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24"  aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button><button class="delete-btn" id="delete-btn/${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg></button><button class="move-btn" id="move-btn/${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg></button></li>`;
       todospendingTemplate += liTemplate;
     });
     todoscompleted.forEach((todo) => {
-      const liTemplate = `<li><span>${todo.content}</span><button class="edit-btn" id="edit-btn/${todo.id}">edit</button><button class="delete-btn" id="delete-btn/${todo.id}">delete</button><button class="move-btn" id="move-btn/${todo.id}">move</button></li>`;
+      const liTemplate = `<li><span>${todo.content}</span><button class="edit-btn" id="edit-btn/${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24"  aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></button><button class="delete-btn" id="delete-btn/${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg></button><button class="move-btn" id="move-btn/${todo.id}"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg></button></li>`;
       todoscompletedTemplate += liTemplate;
     });
 
